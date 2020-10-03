2960144779d95ee338e92fd07ae8195c83155623
zoheezus <zohaib0126@gmail.com>
2020-10-03 11:46:14 -0400

    linting

--- src/poseapp/posegeom.py
+++ src/poseapp/posegeom.py
@@ -3,29 +3,31 @@ from queue import Queue
 import math
 import numpy as np
 
+
 class PoseGeom:
-  # using coco dataset
+    # using coco dataset
 
-  LEFT_SHOULDER = 2
-  LEFT_ELBOW = 3
-  LEFT_HAND = 4
-  RIGHT_SHOULDER = 5
-  RIGHT_ELBOW = 6
-  RIGHT_HAND = 7
+    LEFT_SHOULDER = 2
+    LEFT_ELBOW = 3
+    LEFT_HAND = 4
+    RIGHT_SHOULDER = 5
+    RIGHT_ELBOW = 6
+    RIGHT_HAND = 7
 
-  LIST_OF_JOINTS = [LEFT_SHOULDER, LEFT_ELBOW, LEFT_HAND, RIGHT_SHOULDER, RIGHT_ELBOW, RIGHT_HAND]
+    LIST_OF_JOINTS = [LEFT_SHOULDER, LEFT_ELBOW,
+                      LEFT_HAND, RIGHT_SHOULDER, RIGHT_ELBOW, RIGHT_HAND]
 
-  @classmethod
-  def angle_btw_2_points(cls, joint_A, joint_B):
-    # becuase y coor increases from top to bottom, we swtich to y
-    # x1 y1 are the points along horizontal axis
-    # https://stackoverflow.com/questions/2676719/calculating-the-angle-between-the-line-defined-by-two-points
-    angle_rads = math.atan2(joint_B.y - joint_A.y, joint_B.x - joint_A.x)
-    angle_rads = -angle_rads
-    return math.degrees(angle_rads)
+    @classmethod
+    def angle_btw_2_points(cls, joint_A, joint_B):
+        # becuase y coor increases from top to bottom, we swtich to y
+        # x1 y1 are the points along horizontal axis
+        # https://stackoverflow.com/questions/2676719/calculating-the-angle-between-the-line-defined-by-two-points
+        angle_rads = math.atan2(joint_B.y - joint_A.y, joint_B.x - joint_A.x)
+        angle_rads = -angle_rads
+        return math.degrees(angle_rads)
 
-  @classmethod
-  def distance_btw_2_points(cls, joint_A, joint_B):
-    a = np.array((joint_A.x, joint_A.y))
-    b = np.array((joint_B.x, joint_B.y))
-    return np.linalg.norm(a - b)
+    @classmethod
+    def distance_btw_2_points(cls, joint_A, joint_B):
+        a = np.array((joint_A.x, joint_A.y))
+        b = np.array((joint_B.x, joint_B.y))
+        return np.linalg.norm(a - b)
