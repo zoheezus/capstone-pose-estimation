96f8893e2f16ad1a3252aa25784f64f4128c846d
zoheezus <zohaib0126@gmail.com>
2020-10-04 12:59:58 -0400

    recieving thread function

--- src/util/framesocketstream.py
+++ src/util/framesocketstream.py
@@ -46,3 +46,56 @@ class FrameSocketStream():
             target=self._th_recv, args=(recv_callback,))
         self.th_recv.daemon = True
         self.th_recv.start()
+
+    def _th_recv(self, callback):
+      data = b''
+      payload_size = struct.calcsize("<L")
+      while True:
+        try:
+          while len(data) < payload_size:
+            data += self.socket.recv(8192)
+
+          logger.debug("Frame size received. Size: {}\n".format(len(data)))
+          packed_msg_size = data[:payload_size]
+          msg_payload_size = struct.unpack("<L", packed_msg_size)[0]
+
+          # Get human data
+          data = data[payload_size:]
+          while len(data) < msg_payload_size:
+            data += self.socket.recv(8192)
+
+          logger.debug("Frame received. Size: {}\n".format(len(data)))
+        except socket.timeout:
+          # auto close socket
+          logger.info("Socket timeout at recv thread. Continuing...")
+          continue
+
+        except Exception:
+          logger.error("Exception occured: {}\n".format(traceback.format_exc()))
+          break
+
+        msg = data[:msg_payload_size]
+        # check if server is sending close signal
+        if msg == b'close':
+          logger.info("received closed from server")
+          if not self.socket_is_closed:
+            try:
+              self.socket.shutdown(2)
+              self.socket.close()
+              logger.infor("Socket closed")
+              self.socket_is_closed = True
+
+            except Exception as e:
+              logger.error("Error closing socket. {}".format(traceback))
+          break
+
+        try:
+          data = data[msg_payload_size:]
+          # convert the frame and human data back to its original form
+          frame, pose = pickle.loads(msg)
+          callback(frame, pose)
+        except Exception as e:
+          logger.error(traceback.format_exc())
+          continue
+
+      logger.info("Exiting receiving thread")
