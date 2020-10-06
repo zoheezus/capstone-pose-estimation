7bb51d7f5eef280cd4aa0d4a308506d4c438e26a
zoheezus <zohaib0126@gmail.com>
2020-10-06 08:50:16 -0400

    imports

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+import logging
 import threading
+import traceback
 from queue import Empty
 
 import cv2
 from flask import Flask, render_template, Response, request
 from flask_socketio import SocketIO, send, emit
 
+from src.poseapp.poseapp_sockets import PoseAppWSockets
+
 app = Flask(__name__, template_folder="static")
 socketio = SocketIO(app)
 processing_started = False
+logger = logging.getLogger(__name__)
+
+poseapp = PoseAppWSockets(delay_time=160)
+
+@app.route
 
 @app.route('/')
 def index():
