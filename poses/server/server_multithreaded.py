21eb7ad0e45c3f9fabed3e14dde06677ef90891a
zoheezus <zohaib0126@gmail.com>
2020-10-18 21:15:07 -0400

    port

--- poses/server/server_multithreaded.py
+++ poses/server/server_multithreaded.py
@@ -29,3 +29,27 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+n_workers = 2
+print("Using 2 workers for thread pool")
+futures_q = Queue(maxsize=n_workers)
+worker_mgr = None
+th_signal = threading.Event()
+process_th = None
+send_th = None
+exc_info = None
+exc_thrown = False
+
+estimator = TfPoseEstimator(get_graph_path("mobilenet_thin"), target_size=(432, 368),
+                            tf_config=tf.ConfigProto(log_device_placement=True))
+
+w, h = model_wh("432x368")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+HOST = '0.0.0.0'
+PORT = 8089
+conn = None
+addr = None
+connected = False
